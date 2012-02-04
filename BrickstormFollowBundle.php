0fc07fa333073e4d5f5181e4a367e8e443f2235a
bbergstorm <bbergstorm@gmail.com>
2012-02-04 16:54:12 +0100

    first commit

---
+++ BrickstormFollowBundle.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * This file is part of the <name> project.
+ *
+ * (c) <yourname> <youremail>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Brickstorm\FollowBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+
+/**
+ * References :
+ *   bundles : http://symfony.com/doc/current/book/bundles.html
+ *
+ * @author <Benoît Bergstörm> <b@brickstorm.fr>
+ */
+class BrickstormFollowBundle extends Bundle
+{
+}
